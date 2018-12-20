c4bf4126633cd2c753863f68e78f005ca226b309
flamingows <46027038+flamingows@users.noreply.github.com>
2018-12-20 23:38:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,24 @@
-//var feed = document.getElementById("image-feed");
-//var images = document.getElementsByTagName("img");
-const selectedFile = document.getElementById('input').files[0];
+var CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dxoittcx2/upload';
+var CLOUDINARY_UPLOAD_PRESET = 'wko4ld72';
 
-input.addEventListener("change", handleFiles, false);
-//document.getElementById('elin2').innerHTML = 5*6;
-function handleFiles(files) {
-  document.getElementById('elin2').innerHTML = 'This <strong>Works!</strong>';
-  const fileList = this.files; /* now you can work with the file list */
-  const numFiles = fileList.length;
-  console.log(fileList[0].name);
-  for (let i = 0, numFiles = files.length; i < numFiles; i++) {
-  const file = files[i];
-}
-    const file = fileList[0];
-    console.log(file);
-    saveAs(file, fileList[0].name);
-    document.getElementById('test').src = fileList[0].name;//forandrer image source til dette
-}
+var fileUpload = document.getElementById('input');
 
+input.addEventListener("change", function(event){
+  var file = event.target.files[0];
+  var formData = new FormData();
+  formData.append('file', file);
+  formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
 
-
-//old stuff
-/*const input = document.querySelector('input[type="file"]')
-input.addEventListener('change', function (e) {
-  console.log(input.files)
-  const reader = new FileReader ()
-  reader.onload = function () {
-    console.log(reader.result)
-
-    const fs = require('fs');
-    fs.writeFile(reader.result, photos, (err) => {
-    // throws an error, you could also catch it here
-    if (err) throw err;
-
-    // success case, the file was saved
-    console.log('Saved!');
+  axios({
+    url: CLOUDINARY_URL,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data: formData;
+  }).then(function(res) {
+    console.log(res);
+  }).catch(function(err) {
+    console.error(err);
+  });
 });
-  }
-  reader.readAsText(input.files[0])
-}, false)
-*/
